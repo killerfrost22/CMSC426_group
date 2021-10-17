a5b9aecadada4553b3abbe753ee0bcdfe31bb0d7
sonofmammon <sonofmammon@gmail.com>
2021-10-16 20:50:19 -0400

    anms debug

--- Project2/Code/pan_temp.m
+++ Project2/Code/pan_temp.m
@@ -21,15 +21,15 @@ next_img = imread(location + "2.jpg");
 
      curr_img_corners = cornermetric(curr_img_grayscale);
      next_img_corners = cornermetric(next_img_grayscale);
-     
+
      % imregional 
      curr_img_maxes = imregionalmax(curr_img_corners);
      next_img_maxes = imregionalmax(next_img_corners);
 
      % ANMS
      n_best = 500; 
-     [curr_img_x_best, curr_img_y_best] = ANMS(n_best, curr_img, curr_img_maxes, curr_img_corners)
-     [next_img_x_best, next_img_y_best] = ANMS(n_best, next_img, next_img_maxes, next_img_corners)
+     [curr_img_x_best, curr_img_y_best] = ANMS(n_best, curr_img, curr_img_corners, curr_img_maxes)
+     [next_img_x_best, next_img_y_best] = ANMS(n_best, next_img, next_img_corners, next_img_maxes)
 
      % Display 
        displayANMS(curr_img, curr_img_x_best, curr_img_y_best);
@@ -56,37 +56,38 @@ next_img = imread(location + "2.jpg");
 
     hImage = showMatchedFeatures(curr_img, next_img, [INLIERSp1X, INLIERSp1Y], [INLIERSp2X, INLIERSp2Y], 'montage')
 
-    function [x_best, y_best] = ANMS(n_best, img, features, SHOW_OUTPUT)
+    function [x_best, y_best] = ANMS(n_best, img, img_corners, img_maxes)
     
     NStrong = 0;
-    [y_size, x_size, ~] = size(img);
-    x_max = [];
-    y_max = [];
+    [ x_size, y_size,  ~] = size(img);
+    x = [];
+    y = [];
+
+%     surf(img_corners)
+%     imshow(img_maxes);
+
+%     imregionalmax is inverted
+%     cornermetric data is not inverted
 
 
-    for x = 1:x_size
-        for y = 1:y_size
-            if features(y,x) == 1
+    for x_i = 1:x_size
+        for y_i = 1:y_size
+            if img_maxes(x_i,y_i) == 1
                 NStrong = NStrong + 1;
-                x_max = [x_max x];
-                y_max = [y_max y];  % array concatenation
+%                 x_max = [x_max x];
+%                 y_max = [y_max y];  % array concatenation
+                x = [x y_i];
+                y = [y x_i];  % array concatenation
             end
         end
     end
     
-%     if SHOW_OUTPUT
-%         % Plot features
-%         imshow(img)
-%         hold on
-%         plot(features)
-%         hold off
-%     end
-    
     r = Inf(1, NStrong);
     for i = 1:NStrong
         for j = 1:NStrong
-            if SHOW_OUTPUT(y_max(j), x_max(j)) > SHOW_OUTPUT(y_max(i), x_max(i))
-                ED = ((x_max(j) - x_max(i))^2) + ((y_max(j) - y_max(i))^2);
+%             if img_corners(y_max(j), x_max(j)) > img_corners(y_max(i), x_max(i))
+            if img_corners(x(j), y(j)) > img_corners(x(i), y(i))
+                ED = ((x(j) - x(i))^2) + ((y(j) - y(i))^2);
                 if ED < r(i)
                     r(i) = ED;
                 end
@@ -100,15 +101,10 @@ next_img = imread(location + "2.jpg");
     % lowest n_best points
     [~, I] = sort(r, 'descend');
     
-%         x = x(idx(1:NBest));
-%     y = y(idx(1:NBest));
-    
-    p = [x(:), y(:)];
-    
     for i = 1:n_best
         indx = I(i);
-        x_best(i) = x_max(indx);
-        y_best(i) = y_max(indx);
+        x_best(i) = x(indx);
+        y_best(i) = y(indx);
     end
 end
 
@@ -124,7 +120,7 @@ end
 
 function [descriptors] = feature_descriptors(img_grayscale, x_best, y_best)
     [~, n_best] = size(x_best);
-    [y_size, x_size] = size(img_grayscale);
+    [ x_size, y_size] = size(img_grayscale);
     descriptors = [];
     for i = 1:n_best
         % Patch is of size 41x41, so point is the actual center
