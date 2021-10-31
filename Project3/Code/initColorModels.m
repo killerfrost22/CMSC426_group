ea115ce94236f0bf42fd4b3fd7482feed663b6c0
GYQ888121 <1063735714@qq.com>
2021-10-31 11:30:45 -0400

    Update initColorModels.m

--- Project3/Code/initColorModels.m
+++ Project3/Code/initColorModels.m
@@ -1,6 +1,7 @@
 function ColorModels = initializeColorModels(IMG, Mask, MaskOutline, LocalWindows, BoundaryWidth, WindowWidth)
 % INITIALIZAECOLORMODELS Initialize color models.  ColorModels is a struct you should define yourself.
 % This is a comment
+% This is another comment
 % Must define a field ColorModels.Confidences: a cell array of the color confidence map for each local window.
 
 end
