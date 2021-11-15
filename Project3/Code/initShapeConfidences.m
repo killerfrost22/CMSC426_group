4a676c0639bc8883d7ecad827dae956a34ca130d
Joseph Ao <36581618+killerfrost22@users.noreply.github.com>
2021-11-15 17:54:33 -0500

    Update initShapeConfidences.m

--- Project3/Code/initShapeConfidences.m
+++ Project3/Code/initShapeConfidences.m
@@ -1,4 +1,15 @@
 function ShapeConfidences = initShapeConfidences(LocalWindows, ColorConfidences, WindowWidth, SigmaMin, A, fcutoff, R)
 % INITSHAPECONFIDENCES Initialize shape confidences.  ShapeConfidences is a struct you should define yourself.
 
+ShapeConfidences.Confidences = cell(size(LocalWindows,1),1);
+for i=1:size(LocalWindows,1)
+    sigma_s = SigmaMin;
+    f_c = ColorModels.Confidences{i};
+    if fcutoff < f_c && f_c <= 1
+        sigma_s = SigmaMin + A*(f_c - fcutoff)^R;
+    end
+    D = ColorModels.D{i};
+    f_s = ones(WindowWidth) - exp(-D.^2./(sigma_s^2));
+    ShapeConfidences.Confidences{i} = f_s;
+end
 end
