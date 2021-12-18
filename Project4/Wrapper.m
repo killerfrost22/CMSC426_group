425973e35aa947ab1236ce807ed7c4c300d8c6bf
Joseph Ao <36581618+killerfrost22@users.noreply.github.com>
2021-12-18 02:28:56 -0500

    gtsam

--- Project4/Wrapper.m
+++ Project4/Wrapper.m
@@ -5,28 +5,27 @@
 clc
 clear all
 close all
-
+   
 %% Add ToolBox to Path eg. ToolboxPath = 'gtsam_toolbox';
+ToolboxPath = 'gtsam_toolbox';
 addpath(ToolboxPath);
 
 %% Load Data
 % Download data from the following link: 
 % https://drive.google.com/open?id=1ZFXZEv4yWgaVDE1JD6-oYL2KQDypnEUU
-load('CalibParams.mat');
+load('Data/CalibParams.mat');
 
 %% Load Data (Two datasets have been provided: DataMapping and DataSquare; ...
 %% Uncomment the one you are using):
-% load('DataMapping.mat');
-% load('DataSquare.mat');
-
-
+load('Data/DataMappingFall2020.mat');
+%load('Data/DataSquareFall2020.mat');
 %% SLAM Using GTSAM (DetAll: is a cell array with AprilTag detections per frame along with the TagID, ...
 %  K: Camera Calibration Parameters, TagSize: AprilTag Size in the real world, ...
 %  LeftImgs: cell array where each cell is a Image, TLeftImgs: Timestamps for LeftImgs)
-[LandMarksComputed, AllPosesComputed] = SLAMusingGTSAM(DetAll, K, TagSize, LeftImgs, TLeftImgs);
+[LandMarksComputed, AllPosesComputed] = SLAMusingGTSAM(DetAll, K, TagSize, TLeftImgs);
 
 
 %% Extra Credit: (Uncomment the bottom line if you are doing the Extra credit part.)
 %  For other variables, refer: https://cmsc426.github.io/2019/proj/p4/
 % [LandMarksComputed, AllPosesComputed] = SLAMusingGTSAM(DetAll, K, TagSize, qIMUToC, TIMUToC,...
-                                                IMU, LeftImgs, TLeftImgs);
+%                                                IMU, LeftImgs, TLeftImgs);
